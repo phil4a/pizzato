038cebd8f3213c83226f13cad28dae559469d632
phil4a <philjke@mail.ru>
2022-10-13 10:07:53 +0600

    add tippy js

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,4 +1,24 @@
 // Подключение функционала "Чертогов Фрилансера"
-import { isMobile } from "./functions.js";
+import { isMobile } from './functions.js';
 // Подключение списка активных модулей
-import { flsModules } from "./modules.js";
+import { flsModules } from './modules.js';
+
+// подсказка в карточке товара - tooltip
+import tippy, { animateFill } from 'tippy.js';
+import 'tippy.js/dist/tippy.css'; // optional for styling
+import 'tippy.js/animations/shift-away.css';
+
+//tooltip
+
+tippy('.card__tip-button', {
+	theme: 'pizzato',
+	placement: 'bottom-end',
+	allowHTML: true,
+	arrow: false,
+	animateFill: true,
+	plugins: [animateFill],
+	inertia: true,
+	content: function (reference) {
+		return reference.querySelector('.tip-template');
+	},
+});
