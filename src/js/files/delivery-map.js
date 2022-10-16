18040483ccfeb622a83722f878c4543d54766e2e
phil <philjke@mail.ru>
2022-10-16 21:42:36 +0600

    add geocoding and placemarks on map

--- src/js/files/delivery-map.js
+++ src/js/files/delivery-map.js
@@ -13,17 +13,38 @@
 // L.marker(frunze).addTo(deliveryMap).bindPopup('Фрунзе, 238').openPopup();
 // L.marker(michurina).addTo(deliveryMap).bindPopup('Мичурина, 12').openPopup();
 
+const deliveryForm = document.querySelector('.info-delivery__form');
+const inputAddress = document.querySelector('.info-delivery__input');
 const nskCoords = [54.981798, 83.007095];
 const zoomLevel = 10;
+//Url KML-файла объектов карты
+const mapObjLink =
+	'https://raw.githubusercontent.com/phil4a/pizzato/master/src/delivery.kml';
 
 ymaps.ready(initMap);
+
 function initMap() {
+	//прикрепляем поиск к input
+	let suggestView = new ymaps.SuggestView('info-delivery__input');
 	// Создание карты.
 	const myMap = new ymaps.Map('map-delivery', {
 		center: nskCoords,
 		zoom: zoomLevel,
 		controls: [],
 	});
+	deliveryForm.addEventListener('submit', function (e) {
+		e.preventDefault();
+		let inputRequest = inputAddress.value;
+		ymaps.geocode(inputRequest).then(function (res) {
+			let obj = res.geoObjects.get(0).geometry.getCoordinates();
+			console.log(obj);
+			let newPlacemark = new ymaps.Placemark(obj);
+			myMap.geoObjects.add(newPlacemark);
+		});
+	});
+	//Отрисовываем полигоны зоны доставки
+	let dataDelivery = ymaps.geoXml.load(mapObjLink);
+	dataDelivery.then(function (res) {
+		myMap.geoObjects.add(res.geoObjects);
+	});
 }
-
-let data = JSON.parse(delivery);
