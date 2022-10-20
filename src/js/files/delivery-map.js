10f1b55a204adbb5c1f931da7d74d60bd1f7fb10
phil <philjke@mail.ru>
2022-10-20 16:16:03 +0600

    contacts map

--- src/js/files/delivery-map.js
+++ src/js/files/delivery-map.js
@@ -1,22 +1,24 @@
-const deliveryForm = document.querySelector('.info-delivery__form');
-const inputAddress = document.querySelector('.info-delivery__input');
-const nskCoords = [54.981798, 83.007095];
-const zoomLevel = 10;
+if (document.querySelector('#map-delivery')) {
+	const deliveryForm = document.querySelector('.info-delivery__form');
+	const inputAddress = document.querySelector('.info-delivery__input');
+	const nskCoords = [54.981798, 83.007095];
+	const zoomLevel = 10;
 
-ymaps.ready(initMap);
+	ymaps.ready(initMap);
 
-function initMap() {
-	// Создание карты.
-	const myMap = new ymaps.Map('map-delivery', {
-		center: nskCoords,
-		zoom: zoomLevel,
-		controls: [],
-	});
+	function initMap() {
+		// Создание карты.
+		const myMap = new ymaps.Map('map-delivery', {
+			center: nskCoords,
+			zoom: zoomLevel,
+			controls: [],
+		});
 
-	deliveryForm.addEventListener('submit', function (e) {
-		e.preventDefault();
-	});
+		deliveryForm.addEventListener('submit', function (e) {
+			e.preventDefault();
+		});
 
-	//прикрепляем поиск к input
-	let suggestView = new ymaps.SuggestView('info-delivery__input');
+		//прикрепляем поиск к input
+		let suggestView = new ymaps.SuggestView('info-delivery__input');
+	}
 }
