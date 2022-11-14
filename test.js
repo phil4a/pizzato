367eb71f1730016f3a10a5efceaf5eeb6e4f419b
phil <philjke@mail.ru>
2022-11-14 16:31:39 +0700

    add new images

---
+++ test.js
@@ -0,0 +1,28 @@
+dataLayer.push({
+	ecommerce: {
+		purchase: {
+			actionField: {
+				id: '2095',
+				goal_id: 235198153,
+				coupon: '2022GIS',
+			},
+			products: [
+				{
+					id: 'Пицца 4 сыра',
+					name: 'Пицца 4 сыра',
+					price: 729,
+					category: 'Римская пицца',
+					quantity: 1,
+				},
+				{
+					id: 'Макарунс',
+					name: 'Макарунс',
+					price: 69.0,
+					category: 'Десерты',
+					variant: 'клубничный 25гр',
+					quantity: 2,
+				},
+			],
+		},
+	},
+});
